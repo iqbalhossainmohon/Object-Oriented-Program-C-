9df659022a9f77d65650003896c90cd0ef2c57b2
iqbalhossainmohon <iqbalhossainmohon3057@gmail.com>
2024-08-29 12:47:22 +0600

    first commit

---
+++ C++problemSolving/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b, c, secondMax;
+
+    // Input three integer
+    std::cout << "Enter Three Integers Numbers: " << std::endl;
+    std::cin >> a >> b >> c;
+
+    if ((a >= b && a <= c) || (a >= c && a <= b)) {
+        secondMax = a;
+    } else if ((b >= a && b <= c) || (b >= c && b <= a)) {
+        secondMax = b;
+    } else {
+        secondMax = c;
+    }
+
+    std::cout << "The second maximum number is: " << secondMax << std::endl;
+    
+    return 0;
+}
